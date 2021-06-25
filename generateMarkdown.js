283a8f6059996c9dd3fc69774c4a4992ec034148
Bernadette McKnight <sissyhanks@yahoo.com>
2021-06-25 17:57:34 -0400

    generate conection

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,77 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license = "none"){
+    return ''
+  } else {
+    return `(https://img.shields.io/badge/license-${response.license}-blue.svg)`}
+  }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license = "none"){
+    return ''
+  } else {
+    return `*[License](#license)`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license = "none"){
+    return ''
+  } else {
+    return `
+    
+    ## License
+    ![Github License] ${renderLicenseBadge}`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(response) {
+  return `# ${response.title}
 
+<!-- ![ALT](/path/​image.ext) -->
+<!-- [LINK NAME](https://www.linkaddress) -->
+
+## Table of Contents
+* [Description](#description)
+* [Contributing](#contributing)
+* [Installation](#installation)
+* [Test](#test)
+* [Usage](#usage)
+* [Contributing](#contributing)
+${renderLicenseLink()}
+* [GitHub](#githubUserName)
+* [Contact](#contact)
+
+## Installation
+${response.installation}
+
+## Usage
+${response.usage}
+
+## Description 
+${response.description}
+
+
+## Contributing
+${response.contributing}
+
+
+## Test
+${response.tests}
+${renderLicenseSection()}
+## Contributing
+${response.contributing}
+
+## Contact
+if you have any questions feel free to reach out by email ${response.questions} or visit my [Github](https://github.com/${response.githubUserName})
 `;
 }
+
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 // TODO: Include packages needed for this application
 const fs = require( "fs" );
 const inquirer = require( "inquirer" );
+const generateMarkdown = require("./generateMarkdown");
 
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled description, , , , , , 
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
-// TODO: Create an array of questions for user input
 
 const questions = [
     {
@@ -33,12 +17,12 @@ const questions = [
         name: 'description',        
     },
     {
-        type: 'editor',
+        type: 'input',
         message: 'What are the installation instructions for your project?',
         name: 'installation',        
     },
     {
-        type: 'editor',
+        type: 'input',
         message: 'What are some examples of how your project can be used?',
         name: 'usage',        
     },
@@ -46,15 +30,15 @@ const questions = [
         type: 'list',
         message: 'Which license would you like to use for this project?',
         name: 'license',  
-        choices: ["Apache", "GNU General Public License", "MIT", "Eclipse"],      
+        choices: ["Apache", "GNU", "MIT", "Eclipse", "none"],      
     },
     {
-        type: 'editor',
+        type: 'input',
         message: 'What are the contribution guidelines for this project?',
         name: 'contributing',        
     },
         {
-        type: 'editor',
+        type: 'input',
         message: 'What are the test instructions for this project?',
         name: 'tests',        
     },
@@ -84,47 +68,15 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions)
     .then(function(response){
-        const reply = `
-# ${response.title} 
+        const reply = generateMarkdown(response);
 
-
-## Table of Contents
-* [Description](#description)
-* [Contributing](#contributing)
-* [Installation](#installation)
-* [Test](#test)
-* [Usage](#usage)
-* [Contributing](#contributing)
-* [License](#license)
-* [GitHub](#githubUserName)
-* [Contact](#contact)
-
-## Description 
-${response.description}
-
-
-## Contributing
-
-
-
-## Test
-## Installation
-## Usage
-## Contributing
-## License
-![Github License](https://img.shields.io/badge/${response.license}-blue.svg)
-
-
-## GitHub 
-###### check teh folloing for profile [Github](https://github.com/${response.githubUserName})
-
-## Contact
-if you have any questions feel free to reach out by email ${response.questions}
-`
 console.log(reply);
-writeToFile("README.md",reply)
+writeToFile("READMEME.md",reply)
     })
 }
+
+// ## License
+// ![Github License](https://img.shields.io/badge/license-${response.license}-blue.svg)
 
 // Function call to initialize app
 init();
